9f144a5edc19cd501fef5bc65fd996c00c729bb1
Janos Vajda <vajdajanos@gmail.com>
2014-12-12 12:08:38 +0000

    first commit

---
+++ src/getImageMagickVersion.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Return by Imagick version number
+* @author Janos Vajda
+* @return string
+*/
+function getImagickVersion(){
+    $imageconvert_version_string="";
+    $version_number=null;
+    $imageconvert_version_string = shell_exec("imconvert -version");
+
+    if ($imageconvert_version_string){
+
+        if (preg_match("#(\d+\.\d+(\.\d+)*)#", $imageconvert_version_string, $match)) {
+            $version_number= str_replace('.', '', $match[1]);
+        }
+    }
+return $version_number;
+}
